47b5bd87a62f692ba9a2a9676ef2ce953812e52d
Piotr <wrociu@gmai.com>
2017-12-14 23:35:13 +0100

    Removed Obsolete Spaces

---
+++ RemoveObsoleteSpaces/RemoveObsoleteSpaces/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveObsoleteSpaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] delim = { ' ' };
+            string strOriginal = "Jestem    jaki   jestem i inny   nie bede.       ";
+            
+            string strOutput = string.Join(" ", strOriginal.Split(delim, StringSplitOptions.RemoveEmptyEntries));
+
+            Console.WriteLine(strOriginal);
+            Console.WriteLine(strOutput);
+            Console.ReadKey();
+        }
+    }
+}
